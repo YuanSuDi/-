b167b36536b48a4f350ebb537ace2c27f0d4247d
tong19950305 <237932061@qq.com>
2018-02-26 17:12:48 +0800

    2233

--- 函数指针/main.c
+++ 函数指针/main.c
@@ -7,10 +7,10 @@ double mysub(int a, int b);
 double myindex2(double(*)(int, int), int, int);
 int myindex(int(*)(int, int), int , int );
 
-int main()
+int main1()
 {
-	printf("%d\n", myindex(myadd, 1, 56));
-	printf("%.2f\n", myindex2(mysub, 3, 2));
+	printf("%d\n", myindex(mymax, 1, 56));
+	printf("%.5f\n", myindex2(mysub, 3, 2));
 	system("pause");
 	return 0;
 }
@@ -48,3 +48,51 @@ double mysub(int a, int b)
 {
 	return a/ (b*1.0);
 }
+
+
+
+
+//----------------------------------首字母转大写-----------------------------------------------------------------
+
+char  buf[100] = { 0 };
+char * bianMax(char * str)
+{
+	//定义一个指针指向字符数组
+	char * bufr = buf;
+	//修改第一个字符
+	*str >= 97 && *str <= 122 ? (*bufr++ = *str - 0x20) : (*bufr++ = *str);
+	//修改后面的字符
+	while (*str++ && ((*str++ == 32 && (*bufr++ = 32)) ? (*str >= 97 && *str <= 122 ? (*bufr++ = *str - 0x20) : (*bufr++ = *str)) : (*bufr++ = *--str)));
+	/*{
+		if (*str == 32)
+		{
+			*bufr++ = *str;
+			str++;
+			*str >= 97 && *str <= 122 ? (*bufr++ = *str - 0x20) : (*bufr++ = *str);
+		}
+		else
+		{
+			*bufr++ = *str;
+		}
+		
+	}*/
+	*--bufr = 0;
+	return buf;
+}
+
+char * myfun(char* (*p)(char *),char * str)
+{
+	return p(str);
+}
+
+int main()
+{
+	printf("%s\n", bianMax("Asgsag Sgsa gfh刚刚fghf"));
+	printf("%s\n", myfun(bianMax, "assa asfsa Afs"));
+	system("pause");
+	return 0;
+}
+
+
+
+
